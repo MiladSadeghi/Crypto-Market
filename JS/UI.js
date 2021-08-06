7832be845f9e74be9187f5afa18c3648252da1f5
Milad Sadeghi <miladsadeghi2323@gmail.com>
2021-08-06 12:03:39 +0430

    create calculateNumb method for calculate Big Number and return as a currency

--- JS/UI.js
+++ JS/UI.js
@@ -11,9 +11,9 @@ class UI {
           <h4>${element.name}</h4>
           <h5>${element.symbol}</h5>
         </td>
-        <td class="price">${element.price}</td>
-        <td class="change">${element['1d'].price_change_pct}</td>
-        <td class="volume">${element['1d'].volume}</td>
+        <td class="price">${this.calculateNumb('price' ,element.price)}</td>
+        <td class="change">${Number(element['1d'].price_change_pct*100).toFixed(2)}%</td>
+        <td class="volume">${this.calculateNumb('volume' ,element['1d'].volume)}</td>
         <td class="cap">${element.market_cap}</td>
         <td class="supply">${element.max_supply}</td>
         <td class="favoritIcon"><i class="fa fa-plus"></i></td>
@@ -21,7 +21,28 @@ class UI {
     `
     i +=1 
     });
+  }
 
-    
+  calculateNumb(type ,numb) {
+    const formatter = new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD',
+      minimumFractionDigits: 0
+    })
+
+    let number = Number(numb)
+
+    if(type === 'volume') {
+      if(number >= 1000000000) {
+        console.log(number);
+        return `${formatter.format(number/1000000000)}B`
+      }
+      if(number >= 1000000) {
+        console.log(number);
+        return `${formatter.format(number/1000000)}M`
+      }
+    }
+
+    return number
   }
 }
