b5c7e48e4db02a4aae5693e7895ae2b7e8cf1208
Milad Sadeghi <miladsadeghi2323@gmail.com>
2021-08-07 00:07:23 +0430

    use Prettier

--- JS/CryptoAPI.js
+++ JS/CryptoAPI.js
@@ -1,14 +1,14 @@
 class CryptoAPI {
   constructor() {
-    this.API = 'ee2f9d281325238627dffb1e5a970ab8d21d71da'
+    this.API = "ee2f9d281325238627dffb1e5a970ab8d21d71da";
   }
 
   async getDataFromAPI(numb) {
-    let url = `https://api.nomics.com/v1/currencies/ticker?key=${this.API}&status=active&per-page=100&page=${numb}`
+    let url = `https://api.nomics.com/v1/currencies/ticker?key=${this.API}&status=active&per-page=100&page=${numb}`;
 
-    const response = await fetch(url)
-    const result = await response.json()
-    
-    return result
+    const response = await fetch(url);
+    const result = await response.json();
+
+    return result;
   }
 }
--- JS/UI.js
+++ JS/UI.js
@@ -1,121 +1,135 @@
 class UI {
   showResult(result) {
-    const row = document.querySelector('tbody')
-    row.innerHTML = ''
-    let i = 1
-    result.forEach(element => {
+    const row = document.querySelector("tbody");
+    row.innerHTML = "";
+    let i = 1;
+    result.forEach((element) => {
       row.innerHTML += `
       <tr class="rowStyle">
         <td class="count hoverStyle">${i}</td>
         <td class="name">
-          <img src="${element.logo_url}" width="35px" alt="${element.id}" title="${element.id}">
+          <img src="${element.logo_url}" width="35px" alt="${
+        element.id
+      }" title="${element.id}">
           <h4 class="hoverStyle">${element.name}</h4>
           <h5 class="hoverStyle">${element.symbol}</h5>
         </td>
-        <td class="price hoverStyle">${this.calculateNumb('price' ,element.price)}</td>
-        <td class="change hoverStyle">${Number(element['1d'].price_change_pct*100).toFixed(2)}%</td>
-        <td class="volume hoverStyle">${this.calculateNumb('market' ,element['1d'].volume)}</td>
-        <td class="cap hoverStyle">${this.calculateNumb('market' ,element.market_cap)}</td>
-        <td class="supply hoverStyle">${this.calculateNumb('market' ,element.max_supply)}</td>
+        <td class="price hoverStyle">${this.calculateNumb(
+          "price",
+          element.price
+        )}</td>
+        <td class="change hoverStyle">${Number(
+          element["1d"].price_change_pct * 100
+        ).toFixed(2)}%</td>
+        <td class="volume hoverStyle">${this.calculateNumb(
+          "market",
+          element["1d"].volume
+        )}</td>
+        <td class="cap hoverStyle">${this.calculateNumb(
+          "market",
+          element.market_cap
+        )}</td>
+        <td class="supply hoverStyle">${this.calculateNumb(
+          "market",
+          element.max_supply
+        )}</td>
         <td class="favoritIcon"><i class="hoverStyle fa fa-plus"></i></td>
       </tr>
-    `
-    i +=1
+    `;
+      i += 1;
     });
   }
 
-  calculateNumb(type ,numb) {
-    const formatter = new Intl.NumberFormat('en-US', {
-      style: 'currency',
-      currency: 'USD',
-      minimumFractionDigits: 3
-    })
+  calculateNumb(type, numb) {
+    const formatter = new Intl.NumberFormat("en-US", {
+      style: "currency",
+      currency: "USD",
+      minimumFractionDigits: 3,
+    });
 
-    let number = Number(numb)
+    let number = Number(numb);
 
-    if(type === 'market') {
-      if(number >= 1000000000) {
-        return `${formatter.format(number/1000000000)}B`
+    if (type === "market") {
+      if (number >= 1000000000) {
+        return `${formatter.format(number / 1000000000)}B`;
       }
-      if(number >= 1000000) {
-        return `${formatter.format(number/1000000)}M`
+      if (number >= 1000000) {
+        return `${formatter.format(number / 1000000)}M`;
       }
     }
 
-    return formatter.format(number)
+    return formatter.format(number);
   }
 
   favoritList(image, name, price) {
-    const favoritContent = document.querySelector('.favorit-content')
-    const favorit = document.querySelector('.favorit')
+    const favoritContent = document.querySelector(".favorit-content");
+    const favorit = document.querySelector(".favorit");
 
-    const div = document.createElement('div')
-    div.classList.add('favorit-card')
-    div.setAttribute('type', name)
+    const div = document.createElement("div");
+    div.classList.add("favorit-card");
+    div.setAttribute("type", name);
 
     div.innerHTML = `
     <img src=${image} width="35px" alt="${name}" title="${name}">
     <h3>${name}</h3>
     <p class="price">${price}</p>
     <i class="remove fa fa-plus"></i>
-    `
-    favoritContent.appendChild(div)
+    `;
+    favoritContent.appendChild(div);
   }
 
   deleteFavorite(row) {
-    if(row.target.classList.contains('remove')) {
-      row.target.parentElement.remove()
+    if (row.target.classList.contains("remove")) {
+      row.target.parentElement.remove();
     }
   }
 
   pagenation(page) {
-    const row = document.querySelector('tbody')
-    if(page.classList.contains('next')) {
-      let nextValue = Number(page.getAttribute('value')) + 1 
-      page.setAttribute('value', nextValue)
-      page.innerHTML = nextValue
+    const row = document.querySelector("tbody");
+    if (page.classList.contains("next")) {
+      let nextValue = Number(page.getAttribute("value")) + 1;
+      page.setAttribute("value", nextValue);
+      page.innerHTML = nextValue;
 
-      let nowValue = nextValue - 1
-      let now = page.parentElement.children[1]
-      console.log(nowValue);
-      now.setAttribute('value', nowValue)
-      now.innerHTML = nowValue
+      let nowValue = nextValue - 1;
+      let now = page.parentElement.children[1];
+      now.setAttribute("value", nowValue);
+      now.innerHTML = nowValue;
 
-      let prev = page.parentElement.children[0]
-      let prevValue = nowValue - 1
-      prev.setAttribute('value', prevValue)
-      prev.innerHTML = prevValue
-      prev.style.display = 'flex'
+      let prev = page.parentElement.children[0];
+      let prevValue = nowValue - 1;
+      prev.setAttribute("value", prevValue);
+      prev.innerHTML = prevValue;
+      prev.style.display = "flex";
 
-      return nextValue
+      return nextValue;
     }
 
-    if(page.classList.contains('prev')) {
-      let next = page.parentElement.children[2]
-      let nextValue = Number(next.getAttribute('value')) - 1
-      next.setAttribute('value', nextValue)
-      next.innerHTML = nextValue
+    if (page.classList.contains("prev")) {
+      let next = page.parentElement.children[2];
+      let nextValue = Number(next.getAttribute("value")) - 1;
+      next.setAttribute("value", nextValue);
+      next.innerHTML = nextValue;
 
-      let nowValue = nextValue - 1
-      let now = page.parentElement.children[1]
-      now.setAttribute('value', nowValue)
-      now.innerHTML = nowValue
+      let nowValue = nextValue - 1;
+      let now = page.parentElement.children[1];
+      now.setAttribute("value", nowValue);
+      now.innerHTML = nowValue;
 
-      let prev = page.parentElement.children[0]
-      let prevValue = nowValue - 1
-      prev.setAttribute('value', prevValue)
-      prev.innerHTML = prevValue
+      let prev = page.parentElement.children[0];
+      let prevValue = nowValue - 1;
+      prev.setAttribute("value", prevValue);
+      prev.innerHTML = prevValue;
 
-      if(prevValue === 0) {
-        prev.style.display = 'none'
+      if (prevValue === 0) {
+        prev.style.display = "none";
       }
 
-      return prevValue
+      return prevValue;
     }
 
-    if(page.classList.contains('now')) {
-      return false
+    if (page.classList.contains("now")) {
+      return false;
     }
   }
-  
 }
