1d75e3ca19f810eeb978845ff23e960c95e38e74
Milad Sadeghi <miladsadeghi2323@gmail.com>
2021-08-06 19:48:34 +0430

    if favorite-card clicked, send data of clicked section to deleteFavorite(the methode of userInterFace)

--- JS/main.js
+++ JS/main.js
@@ -6,17 +6,20 @@ document.addEventListener('DOMContentLoaded', () => {
   const resultAPI = cryptoAPI.getDataFromAPI()
   .then((data) => {
     userInterFace.showResult(data)
+  })
+  
+  const addToFavorit = document.querySelector('tbody')
+  addToFavorit.addEventListener('click', (e) => {
+    if(e.target.classList.contains('fa')) {
+      const image = e.target.parentElement.parentElement.children[1].children[0].src
+      const name = e.target.parentElement.parentElement.children[1].children[1].textContent
+      const price = e.target.parentElement.parentElement.children[2].textContent
+      userInterFace.favoritList(image, name, price)
+    }
 
-    const addToFavorit = document.querySelector('tbody')
-
-    
-    addToFavorit.addEventListener('click', (e) => {
-      if(e.target.classList.contains('fa')) {
-        const image = e.target.parentElement.parentElement.children[1].children[0].src
-        const name = e.target.parentElement.parentElement.children[1].children[1].textContent
-        const price = e.target.parentElement.parentElement.children[2].textContent
-        userInterFace.favoritList(image, name, price)
-      }
+    const remove = document.querySelector('.favorit-card')
+    remove.addEventListener('click', (e) => {
+      userInterFace.deleteFavorite(e)
     })
   })
 })
