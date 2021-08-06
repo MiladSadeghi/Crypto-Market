3f01a56a1416e615278aee2b0ae021fe7df1f3d9
Milad Sadeghi <miladsadeghi2323@gmail.com>
2021-08-07 00:07:02 +0430

    after click on page button and show result, page scroll to top

--- JS/main.js
+++ JS/main.js
@@ -1,36 +1,39 @@
-const userInterFace = new UI()
-const cryptoAPI = new CryptoAPI()
+const userInterFace = new UI();
+const cryptoAPI = new CryptoAPI();
 
 let pageNumber;
 
-document.addEventListener('DOMContentLoaded', () => {
-  
-  const page = document.querySelector('.page')
-  page.addEventListener('click', (e) => {
-    pageNumber = userInterFace.pagenation(e.target)
-    const resultAPI = cryptoAPI.getDataFromAPI(pageNumber)
-  .then((data) => {
-    userInterFace.showResult(data)
-    })
-  })
+document.addEventListener("DOMContentLoaded", () => {
+  const page = document.querySelector(".page");
+  page.addEventListener("click", (e) => {
+    pageNumber = userInterFace.pagenation(e.target);
+    const resultAPI = cryptoAPI.getDataFromAPI(pageNumber).then((data) => {
+      userInterFace.showResult(data);
+      document.body.scrollTop = 0;
+      document.documentElement.scrollTop = 0;
+    });
+  });
 
-  const resultAPI = cryptoAPI.getDataFromAPI()
-  .then((data) => {
-    userInterFace.showResult(data)
-  })
-  
-  const addToFavorit = document.querySelector('tbody')
-  addToFavorit.addEventListener('click', (e) => {
-    if(e.target.classList.contains('fa')) {
-      const image = e.target.parentElement.parentElement.children[1].children[0].src
-      const name = e.target.parentElement.parentElement.children[1].children[1].textContent
-      const price = e.target.parentElement.parentElement.children[2].textContent
-      userInterFace.favoritList(image, name, price)
+  const resultAPI = cryptoAPI.getDataFromAPI().then((data) => {
+    userInterFace.showResult(data);
+  });
+
+  const addToFavorit = document.querySelector("tbody");
+  addToFavorit.addEventListener("click", (e) => {
+    if (e.target.classList.contains("fa")) {
+      const image =
+        e.target.parentElement.parentElement.children[1].children[0].src;
+      const name =
+        e.target.parentElement.parentElement.children[1].children[1]
+          .textContent;
+      const price =
+        e.target.parentElement.parentElement.children[2].textContent;
+      userInterFace.favoritList(image, name, price);
     }
 
-    const remove = document.querySelector('.favorit-card')
-    remove.addEventListener('click', (e) => {
-      userInterFace.deleteFavorite(e)
-    })
-  })
-})
+    const remove = document.querySelector(".favorit-card");
+    remove.addEventListener("click", (e) => {
+      userInterFace.deleteFavorite(e);
+    });
+  });
+});
