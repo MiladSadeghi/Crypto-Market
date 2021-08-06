50b236966e3107179d5801c2dad9756702010a3c
Milad Sadeghi <miladsadeghi2323@gmail.com>
2021-08-06 17:03:55 +0430

    if favorit icon clicked, get the data from line where the favorite icon was and send it to method of userInferFace class

--- JS/main.js
+++ JS/main.js
@@ -6,5 +6,17 @@ document.addEventListener('DOMContentLoaded', () => {
   const resultAPI = cryptoAPI.getDataFromAPI()
   .then((data) => {
     userInterFace.showResult(data)
+
+    const addToFavorit = document.querySelector('tbody')
+
+    
+    addToFavorit.addEventListener('click', (e) => {
+      if(e.target.classList.contains('fa')) {
+        const image = e.target.parentElement.parentElement.children[1].children[0].src
+        const name = e.target.parentElement.parentElement.children[1].children[1].textContent
+        const price = e.target.parentElement.parentElement.children[2].textContent
+        userInterFace.favoritList(image, name, price)
+      }
+    })
   })
 })
