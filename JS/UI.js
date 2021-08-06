822f40ed15c92c0682adc005809bd34cca0b4a28
Milad Sadeghi <miladsadeghi2323@gmail.com>
2021-08-06 14:30:00 +0430

    add tbody style to row

--- JS/UI.js
+++ JS/UI.js
@@ -5,18 +5,18 @@ class UI {
     result.forEach(element => {
       row.innerHTML += `
       <tr class="rowStyle">
-        <td class="count">${i}</td>
+        <td class="count hoverStyle">${i}</td>
         <td class="name">
           <img src="${element.logo_url}" width="35px" alt="${element.id}" title="${element.id}">
-          <h4>${element.name}</h4>
-          <h5>${element.symbol}</h5>
+          <h4 class="hoverStyle">${element.name}</h4>
+          <h5 class="hoverStyle">${element.symbol}</h5>
         </td>
-        <td class="price">${this.calculateNumb('price' ,element.price)}</td>
-        <td class="change">${Number(element['1d'].price_change_pct*100).toFixed(2)}%</td>
-        <td class="volume">${this.calculateNumb('market' ,element['1d'].volume)}</td>
-        <td class="cap">${this.calculateNumb('market' ,element.market_cap)}</td>
-        <td class="supply">${this.calculateNumb('market' ,element.max_supply)}</td>
-        <td class="favoritIcon"><i class="fa fa-plus"></i></td>
+        <td class="price hoverStyle">${this.calculateNumb('price' ,element.price)}</td>
+        <td class="change hoverStyle">${Number(element['1d'].price_change_pct*100).toFixed(2)}%</td>
+        <td class="volume hoverStyle">${this.calculateNumb('market' ,element['1d'].volume)}</td>
+        <td class="cap hoverStyle">${this.calculateNumb('market' ,element.market_cap)}</td>
+        <td class="supply hoverStyle">${this.calculateNumb('market' ,element.max_supply)}</td>
+        <td class="favoritIcon"><i class="hoverStyle fa fa-plus"></i></td>
       </tr>
     `
     i +=1 
