b4dd6da91c7251a13a8e6c949a648f5585133eb7
Milad Sadeghi <miladsadeghi2323@gmail.com>
2021-08-07 00:01:10 +0430

    add page query to api

--- JS/CryptoAPI.js
+++ JS/CryptoAPI.js
@@ -3,8 +3,8 @@ class CryptoAPI {
     this.API = 'ee2f9d281325238627dffb1e5a970ab8d21d71da'
   }
 
-  async getDataFromAPI() {
-    let url = `https://api.nomics.com/v1/currencies/ticker?key=${this.API}&status=active&per-page=100`
+  async getDataFromAPI(numb) {
+    let url = `https://api.nomics.com/v1/currencies/ticker?key=${this.API}&status=active&per-page=100&page=${numb}`
 
     const response = await fetch(url)
     const result = await response.json()
