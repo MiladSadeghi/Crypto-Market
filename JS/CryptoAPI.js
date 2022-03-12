f3825b3dcf90df296903463f058e378a676a1fba
MiladSadeghi <miladsadeghi2323@gmail.com>
2022-03-12 22:40:38 +0330

    no more cors error

--- JS/CryptoAPI.js
+++ JS/CryptoAPI.js
@@ -4,11 +4,21 @@ class CryptoAPI {
   }
 
   async getDataFromAPI(numb) {
-    let url = `https://api.nomics.com/v1/currencies/ticker?key=${this.API}&status=active&per-page=100&page=${numb}`;
+    let url = `https://course-search-proxy.herokuapp.com`;
+    let body1 = {
+      urlToGet: "https://api.nomics.com/v1/currencies/ticker?key=ee2f9d281325238627dffb1e5a970ab8d21d71da&status=active&per-page=100&page=1" ,
+      Params: "none" 
+    }
 
-    const response = await fetch(url);
+    const response = await fetch(url, {
+      method: "POST",
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(body1)
+    });
     const result = await response.json();
-
+    console.log(result);
     return result;
   }
 }
